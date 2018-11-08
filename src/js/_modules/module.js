859538ac5e0967b927387582f1a9bf92d66d443e
Daniel Adeyemi <daniel.adeyemi@FFUKN586.farfetch.local>
2018-11-08 10:35:43 +0000

    three demo setup

--- src/js/_modules/module.js
+++ src/js/_modules/module.js
@@ -1,3 +1,31 @@
 'use strict';
 
 console.log('Module Loaded')
+
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+
+var container = document.getElementById( 'three-container' );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+container.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+};
+
+animate();
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 'use strict';
 console.log('im aliveee')
 
+window.THREE = require('./_modules/three');
 window.module = require('./_modules/module');
